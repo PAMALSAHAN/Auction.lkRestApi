7e369fbb46019acf3a7a36b5226122eb1ab86ebf
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-06-09 17:27:31 +0530

    http post works

---
+++ Api/models/Vehicle.cs
@@ -0,0 +1,9 @@
+namespace Api.models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string Title { get; set; }
+    }
+}
