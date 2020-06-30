1f013020fff21dc7306468da2b471ad3c6f948ae
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-06-30 11:56:49 +0530

    change password,change phone number ,put image url

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -1,9 +1,13 @@
+using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using Api.data;
 using Api.models;
 using AuthenticationPlugin;
+using ImageUploader;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -88,9 +92,87 @@ namespace Api.Controllers
                 token_type = token.TokenType,
                 creation_Time = token.ValidFrom,
                 expiration_Time = token.ValidTo,
-                user_id=userEmail.Id 
+                user_id = userEmail.Id
             });
 
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult ChangePassword([FromBody] ChangePassword changePassword)
+        {
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+            var user = _dataContext.UserTbl.FirstOrDefault(u => u.Email == userEmail);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //ita passe dena old password ekai user eke tina password eki same da kiyala balanawa. 
+            if (!SecurePasswordHasherHelper.Verify(changePassword.OldPassword, user.Password))
+            {
+                return Unauthorized("Sorry you can't change the password");
+                //login wenakota tinan ekama change karala ganna puluwan
+            }
+
+            //ita passe body eke tina new password eka user ge password eka witdihata add wenna one.
+            user.Password = SecurePasswordHasherHelper.Hash(changePassword.NewPassword);
+            _dataContext.SaveChanges();
+            return Ok("your password has been changed!");
+
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult ChangePhoneNumber([FromBody] ChangePhoneNumber changePhoneNumber)
+        {
+            //claim karala token eken ganna one
+            var userEmail = User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Email).Value;
+            //data base eka email eka tyeda balanawa
+            var user = _dataContext.UserTbl.FirstOrDefault(u => u.Email == userEmail);
+            //balana conditon eka. 
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Phone = changePhoneNumber.ChangePhone;
+            _dataContext.SaveChanges();
+            return Ok("Phone Number Changed Successfully!");
+
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult EditUserProfile([FromBody] byte[] ImageArray)
+        {
+            //claim karala token eken ganna one
+            var userEmail = User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Email).Value;
+            //data base eka email eka tyeda balanawa
+            var user = _dataContext.UserTbl.FirstOrDefault(u => u.Email == userEmail);
+            //balana conditon eka. 
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //image eka upload karanna tinne
+            var stream = new MemoryStream(ImageArray);
+            var guid = Guid.NewGuid().ToString();
+            var file = $"{guid}.jpg";
+            var folder = "wwwroot";
+            var response = FilesHelper.UploadImage(stream, folder, file);
+            if (!response)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                // ita passe karanna tinne  image eka save karana eka. 
+                user.ImageURL=file;
+                _dataContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created);
+            }
         }
 
     }
